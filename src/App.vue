e586284166ec66ffd4cd59276ec1dbb13420145d
DanyPlaya <danila.lusuy@gmail.com>
2023-11-23 14:33:10 +0300

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  
+  <header>
+    <div>Работает</div>
+
+    
+  </header>
+
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+
+
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import 
+
+const app = createApp(app)
+
+app.use(createPinia())
+
+
+app.mount('#app')
